9d24511526d23f3bbe4c87849362e1724c0e73e5
talafarael <127351345+talafarael@users.noreply.github.com>
2024-06-16 14:01:22 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: '*',
+    origin: 'https://kstech-admin.vercel.app',
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
   });
