a1184166db23572220db40ab04a456e8339e06cc
talafarael <127351345+talafarael@users.noreply.github.com>
2024-06-16 13:56:54 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
     origin: '*',
-    credentials: true,
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
   });
   await app.listen(3000);
 }
